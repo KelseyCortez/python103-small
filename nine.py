2b0d09c737fbfc1afe2f6a71275ba8bd7d88b1b8
KelseyCortez <kelscortez@gmail.com>
2020-04-02 13:55:29 -0400

    updated files

---
+++ nine.py
@@ -0,0 +1,10 @@
+# print a 5x5 square with * characters
+
+side = int(input('Square of stars '))
+
+
+
+for i in range(side):
+    for i in range(side):
+         print('*', end = '  ')
+    print()
---
+++ ten.py
@@ -0,0 +1,8 @@
+# print a nxn square of * characters. prompt user for n
+
+side = int(input('Enter any side of a square: '))
+
+for i in range(side):
+    for i in range(side):
+        print('*', end = '  ')
+    print()
